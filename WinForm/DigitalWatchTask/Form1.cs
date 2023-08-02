3a76fc608928322948b523f49dd0250f40a1b50d
CEK <56dmsrud56@naver.com>
2023-08-02 09:10:44 +0900

    08/01

---
+++ WinForm/DigitalWatchTask/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalWatchTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int count;
+        private bool Timeflow = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Timeflow)
+            {
+                Timeflow = false;
+            }
+            else
+            {
+                Timeflow = true;
+            }
+
+
+            var task = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    button1.Text = "Pause";
+                    if (Timeflow == false)
+                    {
+                        button1.Text = "ReStart";
+                        break;
+                    }
+                    Invoke((Action)(() =>
+                    {
+                        label1.Text = count.ToString();
+                        count++;
+
+                    }));
+                    await Task.Delay(1000);
+                }
+
+            });
+
+
+        }
+    }
+}
---
+++ WinForm/WinEx01/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinEx01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int cnt;
+        private Thread t;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            t = new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Invoke((Action)(() =>
+                    label1.Text = cnt++.ToString()));
+                        Thread.Sleep(1000);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                        break;
+                    }
+                }
+            });
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Interrupt();
+        }
+    }
+}
