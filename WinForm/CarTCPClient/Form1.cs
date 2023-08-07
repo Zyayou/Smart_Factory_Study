5c3c38f3ea1fb71326d3c700ebd83b741f3fd5e4
CEK <56dmsrud56@naver.com>
2023-08-07 09:22:43 +0900

    08/04

---
+++ WinForm/CarTCPClient/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarTCPClient
+{
+    public partial class Form1 : Form
+    {
+        private NetworkStream stream;
+        private TcpClient cilent;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text +"+"+ textBox2.Text;
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            stream.Write(data, 0, data.Length);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string serverip = "127.0.0.1";
+            int port = 13000;
+            cilent = new TcpClient(serverip, port);
+            stream = cilent.GetStream();
+        }
+    }
+}
---
+++ WinForm/CarTCPServer/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarTCPServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            //인터넷 주소 만들기
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            //포트만들기
+            int port = 13000;
+            //서버 객체 만들기
+            TcpListener server = new TcpListener(localAddr, port);
+
+            //시작
+            server.Start();
+            //Console.WriteLine("연결 대기중...");
+            richTextBox1.AppendText("연결 대기중...\n");
+
+            while (true)
+            {
+                //클라이언트 연결, 수락
+                TcpClient client = await server.AcceptTcpClientAsync();
+                Task task = Task.Run(() => HandleClient(client));
+            }
+        }
+        private async void HandleClient(TcpClient client)
+        {
+            //Console.WriteLine("연결 성공!");
+            //richTextBox1.AppendText("연결 성공!");
+            AppendText("연결성공\n");
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] data = new byte[1024];
+                int bytes;
+
+                while ((bytes = await stream.ReadAsync(data, 0, data.Length)) != 0)
+                {
+                    AppendText(Encoding.UTF8.GetString(data, 0, bytes) + "\n");
+                }
+            }
+            client.Close();
+        }
+        private void AppendText(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action<string>(AppendText), text);
+            }
+            else
+            {
+                richTextBox1.AppendText(text);
+
+                if (text.Contains('+'))
+                {
+                    string[] num = text.Split('+');
+                    label1.Text = (int.Parse(num[0]) + int.Parse(num[1])).ToString();
+                }
+            }
+        }
+    }
+}
